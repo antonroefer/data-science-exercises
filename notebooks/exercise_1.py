2ded2b234c98b94083e7997fed0d504abf7aeb1b
antonroefer <anton.roefer@web.de>
2025-04-24 17:17:38 +0200

    Relative file path implemented

--- notebooks/exercise_1.py
+++ notebooks/exercise_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 from scipy.stats import ttest_ind
 
 # Load the CSV file
-file_path = "C:/Users/anton/Documents/Studium/Data Science/data-science-exercises/data/external/wetter.csv"
+file_path = "./../data/external/wetter.csv"
 data = pd.read_csv(file_path)
 
 # Calculate the overall average temperature
